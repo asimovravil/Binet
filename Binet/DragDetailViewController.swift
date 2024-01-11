4381fdaf25ce8bf35a483ac01f9d06538c3847c8
Ravil <asimovravil06@gmail.com>
2024-01-11 23:27:35 +0600

    [BINET] added swift file DragDetailViewController

---
+++ Binet/DragDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DragDetailViewController.swift
+//  Binet
+//
+//  Created by Ravil on 11.01.2024.
+//
+
+import UIKit
+
+class DragDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
