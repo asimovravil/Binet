338faec5a2425cf9ec74bc36a18b81f90018e032
Ravil <asimovravil06@gmail.com>
2024-01-11 21:33:54 +0600

    [BINET] Released DrugService

--- Binet/DrugService.swift
+++ Binet/DrugService.swift
@@ -7,4 +7,28 @@
 
 import Foundation
 
+class DrugService {
+    func fetchDrugs(completion: @escaping (Result<[Drug], Error>) -> Void) {
+        let url = URL(string: "http://shans.d2.i-partner.ru/api/ppp/index/")! 
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: nil)))
+                return
+            }
+
+            do {
+                let drugs = try JSONDecoder().decode([Drug].self, from: data)
+                completion(.success(drugs))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
 
