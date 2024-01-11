ef6f1694367fb0c780be191815d329a3e0fb759b
Ravil <asimovravil06@gmail.com>
2024-01-11 21:29:13 +0600

    [BINET] added two Swift files for API

---
+++ Binet/Drug.swift
@@ -0,0 +1,8 @@
+//
+//  Drug.swift
+//  Binet
+//
+//  Created by Ravil on 11.01.2024.
+//
+
+import Foundation
---
+++ Binet/DrugService.swift
@@ -0,0 +1,8 @@
+//
+//  DrugService.swift
+//  Binet
+//
+//  Created by Ravil on 11.01.2024.
+//
+
+import Foundation
