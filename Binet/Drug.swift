b451ee469d01e2291a6447b7e1ff8efb81f1ec97
Ravil <asimovravil06@gmail.com>
2024-01-11 21:33:16 +0600

    [BINET] Released Drug

--- Binet/Drug.swift
+++ Binet/Drug.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+struct Drug: Codable {
+    let id: Int
+    let image: String
+    let categories: [Category]
+    let name: String
+    let description: String
+    let fields: [Field]
+}
+
+struct Category: Codable {
+    let id: Int
+    let icon: String
+    let image: String
+    let name: String
+}
+
+struct Field {
+    let typesId: Int
+    let type: String
+    let name: String
+    let value: String
+    let image: String
+}
+
+struct DrugResponse: Codable {
+    let drugs: [Drug]
+}
+
+extension Field: Codable {
+    enum CodingKeys: String, CodingKey {
+        case typesId = "types_id"
+        case type
+        case name
+        case value
+        case image
+    }
+}
+
