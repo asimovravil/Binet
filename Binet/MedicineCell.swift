8df40287be1fd781d4167d9801db436f85a3ba25
Ravil <asimovravil06@gmail.com>
2024-01-11 20:53:23 +0600

    [BINET] added medicineCard UI

--- Binet/MedicineCell.swift
+++ Binet/MedicineCell.swift
@@ -11,6 +11,20 @@ final class MedicineCell: UICollectionViewCell {
     
     static let reuseID = String(describing: MedicineCell.self)
     
+    // MARK: - UI
+    
+    private lazy var medicineCard: UIView = {
+        let uiView = UIView()
+        uiView.backgroundColor = .white
+        uiView.layer.cornerRadius = 8
+        uiView.layer.shadowColor = UIColor.black.cgColor
+        uiView.layer.shadowOpacity = 0.15
+        uiView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        uiView.layer.shadowRadius = 4
+        uiView.translatesAutoresizingMaskIntoConstraints = false
+        return uiView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
