e4f1d9062df9e2c6c0caffa817d78bfba808f3f7
Ravil <asimovravil06@gmail.com>
2024-01-11 20:45:10 +0600

    [BINET] created collectionView UI

--- Binet/MedicineViewController.swift
+++ Binet/MedicineViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 final class MedicineViewController: UIViewController {
     
+    // MARK: - UI
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero)
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
